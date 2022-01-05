48c820d67f97ced50dc391792839bd568fa9c63b
Rutger Mauritz <rutger.mauritz@togetr.nl>
2022-01-05 13:31:44 +0100

    Restructured code Made NNH usable in IG setting

--- TSP_greedy/TSP_IG.py
+++ TSP_greedy/TSP_IG.py
@@ -1,19 +1,21 @@
 import random
 from math import inf
 from Helper import compute_cost
+from typing import Callable
 
 
 # Implementation of an iterated greedy algorithm
 
-def IG(xy, dist, L, I):
+def IG(xy, dist, construct: Callable, L, I):
     """"
-    RIH algorithm in an iterative greedy (IG) setting.
+    Iterated Greedy (IG) algorithm
     The algorithm consists of the following phases:
     1) INIT: An initial tour is constructed using RIH
     2) DESTRUCTION: L nodes are removed at random from the tour
-    3) CONSTRUCTION: the removed nodes are again added via RIH
+    3) CONSTRUCTION: the removed nodes are again added via a heuristic such as RIH, NIH, NNH
     4) ACCEPTANCE: if the new solution is better than the old, keep it. Go back to 2) until the number of iterations
                    exceeds I
+    :parameter construct: function that is used to construct a full solution, e.g. RIH, NIH, NNH
     """
 
     C = xy.shape[0]
@@ -35,8 +37,8 @@ def IG(xy, dist, L, I):
             x = random.choice(vis)
             vis.remove(x)
             unvis.append(x)
-        # CONSTRUCTION: use RIH to build up a new solution again
-        vis_new, unvis_new = RIH(dist, vis, unvis)
+        # CONSTRUCTION: use heuristics to build up a new solution again
+        vis_new, unvis_new = construct(dist, vis, unvis)
         # ACCEPTANCE: if the new solution has improved, update the incumbent to the new solution, otherwise keep old
         if compute_cost(dist, vis_new) < compute_cost(dist, vis_old):
             vis_old = vis_new
@@ -61,7 +63,7 @@ def RIH(dist, vis, unvis):
     # If empty or 1 node, there are no nodes to in between which a new node can be inserted
     # Therefore it should be initialized so that there are two nodes at the start
 
-    # INIT (optional)
+    # INIT
     if len(vis) == 0:
         cc = random.sample(unvis, 2)
         vis.extend(cc)
@@ -89,4 +91,69 @@ def RIH(dist, vis, unvis):
         idx = vis.index(c_left)
         vis.insert(idx + 1, c)
         unvis.remove(c)
+    return vis, unvis
+
+
+def NIH(dist, vis, unvis):
+    """"
+    Nearest Insertion Heuristic for the TSP problem
+    """
+
+    # INIT
+    if len(vis) == 0:
+        cc = random.sample(unvis, 2)
+        vis.extend(cc)
+        unvis.remove(cc[0])
+        unvis.remove(cc[1])
+    elif len(vis) == 1:
+        c = random.choice(unvis)
+        vis.extend(c)
+        unvis.remove(c)
+
+    while len(unvis) > 0:
+        min_dist = inf
+        c = unvis[0]  # just an initialization, will probably be overwritten below
+        # find the closest point (c) to all points that are currently in the tour
+        for c1 in vis:
+            for c2 in unvis:
+                if dist[c1, c2] < min_dist:
+                    min_dist = dist[c1, c2]
+                    c = c2
+
+        # Now this point needs to be inserted in between any two points from the tour so that the increase is minimal
+        min_cost = inf
+        c_left = vis[0]  # init, will be possible overwritten below
+        # INSERTION
+        for c1, c2 in zip(vis[:-1], vis[1:]):
+            # cost increase is cost of path c1->nw->c2 minus cost of c1-> c2
+            incr = dist[c1, c] + dist[c, c2] - dist[c1, c2]
+            if incr < min_cost:
+                min_cost = incr
+                c_left = c1
+        # Now insert nw2 in the vis list between nw1 and nw3 in the list + remove from unvis list
+        idx = vis.index(c_left)
+        vis.insert(idx + 1, c)
+        unvis.remove(c)
+    return vis, unvis
+
+
+def NNH(dist, vis, unvis):  # TODO: does not work yet in the IG setting! Goes (?) wrong at min_dist line
+    """"
+    Nearest Neighbourhood Heuristic for the TSP problem
+    """
+
+    if len(vis) == 0:
+        c = random.choice(unvis)
+        vis.append(c)
+        unvis.remove(c)
+
+    # Take as reference point the last added city in the tour
+    c = vis[-1]
+
+    while len(unvis) > 0:
+        # Find the closest unvisited city to the last added city in the tour
+        dist_unvis = [dist[c, i] for i in unvis]
+        c_new = [x for _, x in sorted(zip(dist_unvis, unvis))][0]
+        vis.append(c_new)
+        unvis.remove(c_new)
     return vis, unvis
